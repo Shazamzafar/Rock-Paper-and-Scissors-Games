d8277c55ec04a001c6f8d2a842821a9674173c81
Shazamzafar <47291489+Shazamzafar@users.noreply.github.com>
2019-11-19 18:48:25 -0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace RockPaperZafar
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.Exit_button = new System.Windows.Forms.Button();
+            this.Result_Label = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::RockPaperZafar.Properties.Resources.rock_paper_scissors_emoji_cartoon_007_512;
+            this.pictureBox1.Location = new System.Drawing.Point(23, 47);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(160, 144);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::RockPaperZafar.Properties.Resources.stack_of_paper_vector_293431;
+            this.pictureBox2.Location = new System.Drawing.Point(229, 47);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(160, 144);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_1);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::RockPaperZafar.Properties.Resources.download;
+            this.pictureBox3.Location = new System.Drawing.Point(419, 47);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(160, 144);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click_1);
+            // 
+            // Exit_button
+            // 
+            this.Exit_button.Location = new System.Drawing.Point(131, 311);
+            this.Exit_button.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Exit_button.Name = "Exit_button";
+            this.Exit_button.Size = new System.Drawing.Size(94, 58);
+            this.Exit_button.TabIndex = 3;
+            this.Exit_button.Text = "Exit";
+            this.Exit_button.UseVisualStyleBackColor = true;
+            this.Exit_button.Click += new System.EventHandler(this.Exit_button_Click);
+            // 
+            // Result_Label
+            // 
+            this.Result_Label.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Result_Label.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Result_Label.Location = new System.Drawing.Point(23, 221);
+            this.Result_Label.Name = "Result_Label";
+            this.Result_Label.Size = new System.Drawing.Size(366, 66);
+            this.Result_Label.TabIndex = 4;
+            this.Result_Label.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Location = new System.Drawing.Point(419, 221);
+            this.pictureBox4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(160, 118);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(605, 380);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.Result_Label);
+            this.Controls.Add(this.Exit_button);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Button Exit_button;
+        private System.Windows.Forms.Label Result_Label;
+        private System.Windows.Forms.PictureBox pictureBox4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,118 @@
+﻿//Name:     Shazam Zafar
+//Class:    C#
+//Date:     02/22/2019
+//Project   RockPaperScissors
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace RockPaperZafar
+{
+    public partial class Form1 : Form
+    {
+        //array for elements
+        string[] element = new string[] { "rock", "paper", "scissors" };
+        Random ran = new Random();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //getting the computer choice
+        public int getChoice()
+        {
+            // Getting computer choice and Displaying Picture 
+            int C;
+            C=  ran.Next(1, 4);
+            if (C == 1)
+            {
+                pictureBox4.Image = RockPaperZafar.Properties.Resources.rock_paper_scissors_emoji_cartoon_007_512;
+            }
+            else
+            {
+                if (C == 2)
+                {
+                    pictureBox4.Image = RockPaperZafar.Properties.Resources.stack_of_paper_vector_293431;
+                }
+                else
+                {
+                    if (C == 3)
+
+                    {
+                        pictureBox4.Image = RockPaperZafar.Properties.Resources.download;
+                    }
+
+                }
+            }
+            return C;
+        }
+        //when rock picture was clicked
+        private void pictureBox1_Click_1(object sender, EventArgs e)
+        {
+
+            int c = getChoice();
+        
+            winner(c, 1);
+        }
+        
+        public void winner(int comp, int player)
+        {
+            //if tie
+            if (comp == player)
+            {
+                Result_Label.Text=("its a tie, you both choose " + element[comp - 1]);
+            }
+            //else determaining the winner
+            else if (comp == 1 && player == 2)
+            {
+                Result_Label.Text = ("you choose " + element[player - 1] + " computer choose " + element[comp - 1] + ", you win");
+            }
+            else if (comp == 1 && player == 3)
+            {
+                Result_Label.Text = ("you choose " + element[player - 1] + " computer choose " + element[comp - 1] + ", you lose");
+            }
+            else if (comp == 2 && player == 1)
+            {
+                Result_Label.Text = ("you choose " + element[player - 1] + " computer choose " + element[comp - 1] + ", you lose");
+            }
+            else if (comp == 2 && player == 3)
+            {
+                Result_Label.Text = ("you choose " + element[player - 1] + " computer choose " + element[comp - 1] + ", you win");
+            }
+            else if (comp == 3 && player == 1)
+            {
+                Result_Label.Text = ("you choose " + element[player - 1] + " computer choose " + element[comp - 1] + ", you win");
+            }
+            else if (comp == 3 && player == 2)
+            {
+                Result_Label.Text = ("you choose " + element[player - 1] + " computer choose " + element[comp - 1] + ", you lose");
+            }
+        }
+        //when paper was selected
+        private void pictureBox2_Click_1(object sender, EventArgs e)
+        {
+            int c = getChoice();
+            winner(c, 2);
+        }
+        //when scissors was selected
+        private void pictureBox3_Click_1(object sender, EventArgs e)
+        {
+            int c = getChoice();
+            winner(c, 3);
+        }
+        
+
+        private void Exit_button_Click(object sender, EventArgs e)
+        {
+            // closes the program
+            this.Close();
+        }
+    }
+}
